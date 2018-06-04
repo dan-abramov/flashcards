503bbfdfad43827efd7702a3979a840cb2ef9370
Unknown <mymindsgate@gmail.com>
2018-06-04 17:26:16 +0300

    added 'sign in' feature to controller specs

--- spec/support/helpers/controller_helper.rb
+++ spec/support/helpers/controller_helper.rb
@@ -1,6 +1,5 @@
 module ControllerHelpers
   def sign_in(user)
-    allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
-    allow(controller).to receive(:current_user).and_return(nil)
+    request.session[:user_id] = user.id
   end
 end
