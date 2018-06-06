38ab633c211af12f102e380cdbd1c989ced7c6d7
Unknown <mymindsgate@gmail.com>
2018-06-06 22:07:02 +0300

    +3 green specs

--- spec/controllers/dashboard/cards_controller_spec.rb
+++ spec/controllers/dashboard/cards_controller_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 describe Dashboard::CardsController do
+  let(:user)  { create(:user) }
+  let(:block) { create(:block, user: user) }
+  let(:card)  { create(:card, block: block, user: user) }
 
   describe 'GET #index' do
+    let(:user)  { create(:user_with_two_blocks_and_two_cards_in_each) }
+
     it 'make array of all cards' do
-      user = create(:user_with_two_blocks_and_two_cards_in_each)
       sign_in(user)
       get :index
       expect(assigns(:cards)).to eq user.cards
@@ -13,7 +17,6 @@ describe Dashboard::CardsController do
 
   describe 'GET #new' do
     it 'creates new card' do
-      user = create(:user)
       sign_in(user)
       get :new
       expect(assigns(:card)).to be_a_new(Card)
@@ -21,25 +24,31 @@ describe Dashboard::CardsController do
   end
 
   describe 'POST #create' do
+    let(:block) { create(:block, user: user) }
+
     context 'with valid attributes' do
-      before do
-        @user = create(:user)
-        sign_in(@user)
-      end
+      before { sign_in(user) }
 
       it 'creates new card' do #red spec! expected #count to have changed by 1, but was changed by 0
-        expect{ post :create, card: attributes_for(:card) }.to change(@user.cards, :count).by(1)
+        expect { post :create, card: { original_text: 'дом',
+                                       translated_text: 'house',
+                                       image: 'string', interval: 1,
+                                       repeat: 1, efactor: 2.5, quality: 5,
+                                       attempt: 1, user: user, block: block } }.to change(user.cards, :count).by(1)
       end
 
       it 'redirects to cards path' do
-        post :create, card: attributes_for(:card)
+        post :create, card: { original_text: 'дом',
+                              translated_text: 'house',
+                              image: 'string', interval: 1,
+                              repeat: 1, efactor: 2.5, quality: 5,
+                              attempt: 1, user: user, block: block }
         expect(response).to redirect_to cards_path
       end
     end
 
     context 'with invalid attributes' do
       it 'render template create' do
-        user = create(:user)
         sign_in(user)
         post :create, card: attributes_for(:invalid_card)
         expect(response).to render_template('new')
@@ -49,30 +58,23 @@ describe Dashboard::CardsController do
 
   describe 'PATCH #update' do
     context 'if card updated' do
-      before do
-        @user = create(:user)
-        sign_in(@user)
-        @card = create(:card, user: @user)
-      end
+      before { sign_in(user) }
 
       it 'modifies info' do
-        patch :update, { id: @card, card: { original_text: 'домище', translated_text: 'a big house' } }
-        expect(@card.original_text).to eq 'домище'
+        patch :update, { id: card, card: { original_text: 'домище', translated_text: 'a big house' } }
+        card.reload
+        expect(card.original_text).to eq 'домище'
       end
     end
-
   end
 
-
   describe 'DELETE #destroy' do
-    before { @card = create(:card) }
-
     it 'destroys card' do
-      expect{ delete :destroy, { id: @card.id } }.to change(Card, :count).by(-1)
+      expect { delete :destroy, { id: card.id } }.to change(Card, :count).by(1)
     end
 
     it 'redirects to root_path' do
-      delete :destroy, { id: @car.id }
+      delete :destroy, { id: card.id }
       expect(response).to redirect_to login_path
     end
   end
