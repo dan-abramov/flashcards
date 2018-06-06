043140cb5577b1f3a721e8cfd535980942599ab0
Unknown <mymindsgate@gmail.com>
2018-06-07 01:02:43 +0300

    'shows card with current block' now green

--- spec/controllers/home/home_controller_spec.rb
+++ spec/controllers/home/home_controller_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 describe Home::HomeController do
   describe 'GET #index' do
-    it 'shows card without block' do
-      current_user = create(:user_with_one_block_and_two_cards)
-      sign_in(current_user)
-      get :index, id: current_user.cards[0]
-      expect(assigns(:card)).to eq current_user.cards[0]
+    let(:user) { create(:user_with_one_block_and_two_cards) }
+
+    before { sign_in(user) }
+    it 'shows card without current_block' do
+      get :index, id: user.cards[0]
+      expect(assigns(:card)).to eq user.cards[0]
     end
 
-    it 'shows card with block' do #red spec! undefined method `cards' for nil:NilClass
-      current_user = create(:user_with_current_block_and_two_cards)
-      sign_in(current_user)
+    it 'shows card with current block' do
+      user.current_block = user.blocks[0]
       get :index
-      expect(assigns(:card)).to eq current_user.current_block.cards[0]
+      expect(assigns(:card)).to eq user.current_block.cards[0]
     end
   end
 end
