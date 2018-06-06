811a57d7980ca830432e0c96ecec2b70f841656b
Unknown <mymindsgate@gmail.com>
2018-06-06 22:46:43 +0300

    +2 green specs

--- spec/controllers/dashboard/profile_controller_spec.rb
+++ spec/controllers/dashboard/profile_controller_spec.rb
@@ -1,34 +1,32 @@
 require 'rails_helper'
 
-describe Dashboard::ProfileController do #red specs!
+describe Dashboard::ProfileController do
+  let(:user) { create(:user, password: '123456', password_confirmation: '123456' ) }
+
   describe 'PATCH #update' do
     context 'user updated' do
-      before do
-        @user = create(:user)
-        sign_in(@user)
+      before { sign_in(user) }
+
+      it 'modifies object' do
+        patch :update, { id: user, user: { email: 'new@email.com', password: '123456',
+                         password_confirmation: '123456', locale: 'en' } }
+        user.reload
+        expect(user.email).to eq 'new@email.com'
+        expect(user.locale).to eq 'en'
       end
 
-      it 'modifies object' do  #did not change e-mail and locale
-        patch :update, { id: @user, user: { email: 'new@email.com', password: @user.password,
-                         password_confirmation: @user.password_confirmation, local: 'en' } }
-        @user.reload
-        expect(@user.email).to eq 'new@email.com'
-        expect(@user.local).to eq 'en'
-      end
-
-      it 'redirects to edit_profile_path' do #Expected response to be a <redirect>, but was <200>
-        patch :update, { id: @user, user: { email: 'new@email.com', password: @user.password,
-                         password_confirmation: @user.password_confirmation, local: 'en' } }
+      it 'redirects to edit_profile_path' do
+        patch :update, { id: user, user: { email: 'new@email.com', password: '123456',
+                         password_confirmation: '123456', locale: 'en' } }
         expect(response).to redirect_to edit_profile_path
       end
     end
 
     context 'user did not update' do
       it 'render user template' do
-        user = create(:user)
         sign_in(user)
         patch :update, { id: user, user: { email: 'new@email.com', password: user.password,
-                         password_confirmation: user.password_confirmation, local: 'en' } }
+                         password_confirmation: user.password_confirmation, locale: 'en' } }
         expect(response).to render_template ('edit')
       end
     end
