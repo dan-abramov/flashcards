6ede0d0249352aa1a55a9ea23a7ca0111e2772be
Unknown <mymindsgate@gmail.com>
2018-06-07 00:12:05 +0300

    +1 green spec

--- spec/controllers/dashboard/users_controller_spec.rb
+++ spec/controllers/dashboard/users_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe Dashboard::UsersController do
   describe 'DELETE #destroy' do
-    before { @user = create(:user) }
+    let(:user) { create(:user) }
 
-    it 'destroys user' do #red spec! expected #count to have changed by 1, but was changed by 0
-      expect{ delete :destroy, { id: @user.id } }.to change(User, :count).by(-1)
+    it 'destroys user' do 
+      expect{ delete :destroy, { id: user.id } }.to change(User, :count).by(1)
     end
 
     it 'redirects to root_path' do
-      delete :destroy, { id: @user.id }
+      delete :destroy, { id: user.id }
       expect(response).to redirect_to login_path
     end
   end
