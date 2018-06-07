768debc6bfe2ee2c46efa58633f851d7bed10918
Unknown <mymindsgate@gmail.com>
2018-06-07 23:47:54 +0300

    added traits

--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     title 'Block 1'
     user
 
-    factory :block_with_one_card do
+    trait :with_card do
       after(:create) do |block|
         create(:card, user: block.user, block: block)
       end
     end
 
-    factory :block_with_two_cards do
+    trait :with_2_cards do
       after(:create) do |block|
         create(:card, user: block.user, block: block)
         create(:card, user: block.user, block: block)
