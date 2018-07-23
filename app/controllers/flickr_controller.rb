52e80e931eb2b3dd5eaeb18cd25feb298b11ffbf
Unknown <mymindsgate@gmail.com>
2018-07-23 21:40:47 +0300

    added flickr_controller_spec

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,9 +1,8 @@
 class FlickrController < ApplicationController
+  protect_from_forgery except: :find
+
   def find
     @flickr = Flickr.new('config/flickr.yml')
     @photos = @flickr.photos.get_recent(:tag => params[:flickr].first).take(10)
-    respond_to do |format|
-      format.js
-    end
   end
 end
--- spec/controllers/flickr_controller_spec.rb
+++ spec/controllers/flickr_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe FlickrController, type: :controller do
-
+  before do
+    get :find, params: { flickr: 'hello', format: :js }
+  end
+  describe 'GET #find' do
+    it 'has right api_key and api_secret', :vcr do
+      expect(assigns(:flickr).api_key)   .to eq 'd7e7d59b9148452e69f460ea7be6c4c6'
+      expect(assigns(:flickr).api_secret).to eq '46a323880cc9f589'
+    end
+    
+    it 'has 10 photos', :vcr do
+      expect(assigns(:photos).length).to eq 10
+    end
+  end
 end
