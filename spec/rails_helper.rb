d41137a7515f3127ab22c26750b1bf7babc8578e
Unknown <mymindsgate@gmail.com>
2018-08-23 00:20:05 +0300

    no-sandbox moved from travis.yml to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,10 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 end
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+Capybara.register_driver :chrome do |app|
+	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+
+	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
+
+Capybara.javascript_driver = :chrome
