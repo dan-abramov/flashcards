0b59dc7be9168a9fedc863d52c88ba1dc51cbdb4
Unknown <mymindsgate@gmail.com>
2018-08-24 15:26:16 +0300

    added DRY principle

--- spec/features/card/change_image_in_card_spec.rb
+++ spec/features/card/change_image_in_card_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 require 'support/helpers/login_helper.rb'
 include LoginHelper
 
-describe 'change image to flickr image' do
-  let!(:user) { create(:user, :with_block_with_card) }
+describe 'change picture in card' do
+  let!(:user)   { create(:user, :with_block_with_card) }
 
-  it 'changes image', :vcr, js: true do
+  before do
     visit root_path
     login('test@test.com', '12345', 'Войти')
     visit trainer_path
     expect(current_path).to eq trainer_path
-
     click_on('Изменить изображение')
+  end
+
+  it 'image to flickr_image', :vcr, js: true do
     click_on('Загрузить с Flickr')
     fill_in 'flickr[]', with: 'hello'
     click_on 'Найти'
@@ -29,18 +31,10 @@ describe 'change image to flickr image' do
       page.find(:css,'img[src*="' + image_source + '"]')
     end
   end
-end
 
-describe 'change flickr image to image' do
   let!(:user) { create(:user, :with_block_and_card_with_flickr_image) }
 
-  it 'changes image', :vcr, js: true do
-    visit root_path
-    login('test@test.com', '12345', 'Войти')
-    visit trainer_path
-    expect(current_path).to eq trainer_path
-
-    click_on('Изменить изображение')
+  it 'flickr_image to image', :vcr, js: true do
 
     attach_file("card_image", Rails.root + "spec/support/files/image.png")
     click_on 'Сохранить'
