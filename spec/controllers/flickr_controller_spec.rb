73bd8101be33f98b3800da6af743d84044953f85
Unknown <mymindsgate@gmail.com>
2018-08-24 14:41:38 +0300

    now API key and API secrect keeps in a relative secret

--- spec/controllers/flickr_controller_spec.rb
+++ spec/controllers/flickr_controller_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
+require 'yaml'
 
 RSpec.describe FlickrController, type: :controller do
   before do
     get :find, params: { flickr: 'hello', format: :js }
+    @flickr_access = YAML.load(File.read("config/flickr.yml"))
   end
   describe 'GET #find' do
     it 'has right api_key and api_secret', :vcr do
-      expect(assigns(:flickr).api_key)   .to eq 'd7e7d59b9148452e69f460ea7be6c4c6'
-      expect(assigns(:flickr).api_secret).to eq '46a323880cc9f589'
+      expect(assigns(:flickr).api_key)   .to eq @flickr_access['key']
+      expect(assigns(:flickr).api_secret).to eq @flickr_access['secret']
     end
-    
+
     it 'has 10 photos', :vcr do
       expect(assigns(:photos).length).to eq 10
     end
